b4f60c0ea09dd95b8da3d311a769789521c41366
Nizar Rasyiid <nizarrasyiid1221@gmail.com>
2021-10-07 15:57:57 +0700

    new

--- app/Http/Controllers/VuserController.php
+++ app/Http/Controllers/VuserController.php
@@ -79,9 +79,9 @@ public function showUser(Request $request)
         
         
     $data2 = DB::select(DB::raw('SELECT * FROM setting WHERE "Id_kelompok_data" = 2'));
-    $data3 = DB::select(DB::raw("SELECT * FROM setting WHERE Id_kelompok_data = 8"));
-    $data4 = DB::select(DB::raw("SELECT * FROM setting WHERE Id_kelompok_data = 9"));
-    $alatKB = DB::select(DB::raw("SELECT * FROM setting WHERE Id_kelompok_data = 14"));
+    $data3 = DB::select(DB::raw('SELECT * FROM setting WHERE "Id_kelompok_data" = 8'));
+    $data4 = DB::select(DB::raw('SELECT * FROM setting WHERE "Id_kelompok_data" = 9'));
+    $alatKB = DB::select(DB::raw('SELECT * FROM setting WHERE "Id_kelompok_data" = 14'));
 
     
     try {
